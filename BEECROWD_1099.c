6e40198b61cc6afdbb5bfa9d981adb738d802db5
Filipe Castro <lipecs.1111@gmail.com>
2025-11-26 08:54:52 -0300

    Refatorar lógica de soma e troca de valores no problema BEECROWD 1099

--- BEECROWD_1099.c
+++ BEECROWD_1099.c
@@ -1,38 +1,35 @@
 #include <stdio.h>
 
 int main() {
-
-    int n, x, y;
-    int soma = 0;
+    int n, x, y, temp;
+    // Removemos 'soma' daqui de cima.
 
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++) {
         scanf("%d %d", &x, &y);
-        
-        if(x < y) {
-            for(int j = x + 1; j < y; j++) {
-                if( j % 2 != 0) {
-                    soma += j;
-                }
-            }
-            printf("%d\n", soma);
-            
-            soma = 0;
 
-        } else if(x > y) {
-            for(int j = x - 1; j > y; j--) {
-                if( j % 2 != 0) {
-                    soma += j;
-                }
-            }
-            printf("%d\n", soma);
+        // Declaração e inicialização da soma para ESTE caso de teste
+        int soma = 0;
 
-            soma = 0;
-
-        } else {
-            printf("%d\n", soma);
+        // Lógica de Troca (Swap):
+        // Se X for maior que Y, trocamos os dois.
+        // Assim, X será sempre o menor e Y sempre o maior.
+        if (x > y) {
+            temp = x;
+            x = y;
+            y = temp;
         }
+
+        // Agora só precisamos de um loop, pois sabemos que X < Y (ou são iguais)
+        // Começamos em x + 1 para excluir o próprio x
+        for (int j = x + 1; j < y; j++) {
+            if (j % 2 != 0) {
+                soma += j;
+            }
+        }
+
+        printf("%d\n", soma);
     }
 
     return 0;
